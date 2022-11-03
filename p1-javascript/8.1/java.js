a821d9f95e399fee7229aa0647556bfe1baea776
Ouail06 <9019064@student.zadkine.nl>
2022-11-03 15:42:00 +0100

    8.1

--- p1-javascript/8.1/java.js
+++ p1-javascript/8.1/java.js
@@ -1,2 +1,32 @@
-var getall = parseInt(window.document.getElementById("getall").ariaValueMax, 10);
-function
+/*var getall = parseInt(window.document.getElementById("getall").ariaValueMax, 10);*/
+function myfun()
+{
+    var a=parseInt(document.getElementById('Number').value);
+    var b=parseInt(document.getElementById('Numbers').value);
+    document.getElementById('result').innerHTML=a+b
+
+}
+
+function myfun2()
+{
+    var a=parseInt(document.getElementById('Number').value);
+    var b=parseInt(document.getElementById('Numbers').value);
+    document.getElementById('result').innerHTML=a-b
+
+}
+
+function myfun3()
+{
+    var a=parseInt(document.getElementById('Number').value);
+    var b=parseInt(document.getElementById('Numbers').value);
+    document.getElementById('result').innerHTML=a*b
+
+}
+
+function myfun4()
+{
+    var a=parseInt(document.getElementById('Number').value);
+    var b=parseInt(document.getElementById('Numbers').value);
+    document.getElementById('result').innerHTML=a+b
+
+}
